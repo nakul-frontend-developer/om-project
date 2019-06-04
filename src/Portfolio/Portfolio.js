c2a5ecd10d204e764fb8260c68d930db1987491c
Nakul Kumar <nakulkumar1994@gmail.com>
2019-06-04 18:28:52 +0530

    portfolio js

--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -1,15 +1,60 @@
 import React from 'react';
 import {Helmet} from 'react-helmet';
+import {Container , Row} from 'react-bootstrap';
 
-const Portfolio = () => {
-    return (
+export default class Portfolio extends React.Component  {
+    constructor(props){
+        super(props);
+        this.state = {
+            portfolioList : [
+                {name : 'project test-1', CMS : 'Wordpress'},
+                {name : 'project test-2', CMS : 'Zend'},
+                {name : 'project test-3', CMS : 'Magento'},
+                {name : 'project test-4', CMS : 'Drupal'},
+                {name : 'project test-5', CMS : 'CI'}
+            ],
+            activeState : false
+        }
+
+        this.activeStateHandler = this.activeStateHandler.bind(this)
+    }
+    
+
+    activeStateHandler(e){
+        this.setState((prevState)=>{
+            return {activeState : !prevState.activeState}
+        })
+        //let getId = e.currentTarget.dataset.id
+        //console.log('clicked', getId)
+    }
+
+    render () {
+        const listActiveClass = (this.state.activeState ? 'active' : '');
+        const assignPortfolio = this.state.portfolioList.map((getPortfolio , index) => {
+            return (
+                <li key={index}
+                    onClick={this.activeStateHandler} 
+                    className={listActiveClass}
+                    // className={index === 0 ? 'active' : ''}
+                    data-id={index + 1}>{getPortfolio.name} {getPortfolio.CMS}
+                </li>
+            )
+        })
+        return(
         <React.Fragment>
-        <Helmet>
-        <title>Portfolio</title>
-        </Helmet>
-        <div>Portfolio page content here</div>
+            <Helmet>
+            <title>Portfolio</title>
+            </Helmet>
+            <Container>
+                <Row>
+                    <h5 style={{display:'block', width: '100%'}}><b>Portfolio page content here</b></h5>
+                    <ul>{assignPortfolio}</ul>
+                </Row>
+            </Container>
         </React.Fragment>
-    );
+        )
+    }
+   
+ 
 }
 
-export default Portfolio;
